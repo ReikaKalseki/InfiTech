0b266d460111b09ba1b4d849fc61df0ff54e8617
Reika <reikasminecraft@gmail.com>
2017-07-02 01:49:35 -0400

    Update 2

--- config.lua
+++ config.lua
@@ -1,7 +1,4 @@
 Config = {}
 
 --Enable evolution factor research; each time this research is completed, the evolution factor is slightly reduced.
-Config.evoResearch = true
-
---Enable turret damage and shooting speed research
-Config.turretResearch = true
+Config.evoResearch = settings.startup["evo-research"].value--true
---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+        {
+            type = "bool-setting",
+            name = "evo-research",
+            setting_type = "startup",
+            default_value = true,
+            order = "r",
+			localised_name = "Enable evolution factor research",
+			localised_description = "Enable evolution factor research; each time this research is completed, the evolution factor is slightly reduced.",
+        }
+})
