fd5f6d15797d5c0a4dddd0b09d18cb239f29ed83
Reika <reikasminecraft@gmail.com>
2017-08-08 00:18:06 -0400

    Update 5

--- settings.lua
+++ settings.lua
@@ -5,7 +5,7 @@ data:extend({
             setting_type = "startup",
             default_value = true,
             order = "r",
-			localised_name = "Enable evolution factor research",
-			localised_description = "Enable evolution factor research; each time this research is completed, the evolution factor is slightly reduced.",
+			--localised_name = "Enable evolution factor research",
+			--localised_description = "Enable evolution factor research; each time this research is completed, the evolution factor is slightly reduced.",
         }
 })
